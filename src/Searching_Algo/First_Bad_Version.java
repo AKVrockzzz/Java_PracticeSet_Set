1d93c4da2bb427f82fb77c7e3ec01bda92ff293e
AKVrockzzz <ajaykumarverma5549@gmail.com>
2024-12-03 21:09:30 +0530

    Search Algo
    Leetcode questions- First Bad Version

--- src/Searching_Algo/First_Bad_Version.java
+++ src/Searching_Algo/First_Bad_Version.java
@@ -6,5 +6,22 @@ public class First_Bad_Version {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static int BadVersion(int n, int b) {
+		int lo = 1;
+		int hi = n;
+		int ans = 0;
+		while(lo <= hi) {
+			int mid = lo + (hi - lo)/2;
+			if(isBadVersion(mid) == true) {
+				ans = mid;
+				hi = mid-1;
+			}
+			else {
+				lo = mid + 1;
+			}
+		}
+		return ans;
+	}
 
 }

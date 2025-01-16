1effc3f488381f9426059c0f7d8b6484ac17e958
AKVrockzzz <ajaykumarverma5549@gmail.com>
2025-01-16 11:54:07 +0530

    LinkdedList Implementation
    LinkdedList Implementation

--- src/LinkedList_1/LinkedList.java
+++ src/LinkedList_1/LinkedList.java
@@ -1,5 +1,79 @@
 package LinkedList_1;
 
 public class LinkedList {
-
+	class Node {
+		int val;
+		Node next;
+	}
+	
+	private Node head;
+	private Node tail;
+	private int size;
+	
+	public void AddFirst(int item) {
+		Node nn = new Node();
+		nn.val = item;
+		
+		if(size == 0) {
+			head = nn;
+			tail = nn;
+		}
+		else {
+			nn.next = head;
+			head = nn;
+		}
+		size++;
+	}
+	
+	public void AddLast(int item) {
+		if(size == 0) {
+			AddFirst(item);
+		}
+		else {
+			Node nn = new Node();
+			nn.val = item;
+			tail.next = nn;
+			tail = nn;
+			size++;
+		}
+	}
+	
+	
+	public Node GetNode(int k) throws Exception{
+		if(k < 0 || k >= size) throw new Exception("Bklol k tho shi daal de Budwak khen ka :-(");
+		
+		Node temp = head;
+		for(int i=0; i<k; i++) {
+			temp = temp.next;
+			
+		}
+		return temp;
+	}
+	
+	public int GetFirstValue() {
+		return head.val;
+	}
+	
+	public int GetLastValue() {
+		return tail.val;
+	}
+	
+	public Node GetHead() {
+		return head;
+	}
+	
+	public Node GetTail() {
+		return tail;
+	}
+	
+	
+	public void Display() {
+		Node temp = head;
+		
+		while(temp != null) {
+			System.out.print(temp.val + "-->");
+			temp  = temp.next;
+		}
+		System.out.println();
+	}
 }
--- src/LinkedList_1/LinkedList_Client.java
+++ src/LinkedList_1/LinkedList_Client.java
@@ -2,9 +2,23 @@ package LinkedList_1;
 
 public class LinkedList_Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		LinkedList ll = new LinkedList();
+		ll.AddFirst(10);
+		ll.AddFirst(20);
+		ll.AddFirst(30);
+		ll.AddFirst(40);
+		ll.Display();
+		
+		ll.AddLast(50);
+		ll.Display();
+		
+		System.out.println(ll.GetNode(3).val);
+		System.out.println(ll.GetFirstValue());
+		System.out.println(ll.GetLastValue());
+		System.out.println(ll.GetHead().val);
+		System.out.println(ll.GetTail().val);
 	}
 
 }
